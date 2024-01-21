2a5c10f1628a93021cfd0950c0a65a86f32265cd
Akash Hegde <akash.hegde@viacom18.com>
2024-01-21 22:45:28 +0530

    count bytes in a file

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 build:
 	go build -o ~/bin/ccwc cmd/ccwc/main.go
 
+run:
+	 go run cmd/ccwc/main.go -c test.txt
+
 hello:
 	echo "hello"
 
--- cmd/ccwc/main.go
+++ cmd/ccwc/main.go
@@ -3,11 +3,23 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	var byteCount bool
 	flag.BoolVar(&byteCount, "c", false, "count bytes in the file")
 	flag.Parse()
-	fmt.Println(byteCount)
+
+	if len(os.Args) < 2 {
+		fmt.Println("file input missing")
+		os.Exit(1)
+	}
+	file := os.Args[2]
+	data, err := os.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(len(data), file)
 }

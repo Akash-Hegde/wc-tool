b3342eb62ad3beb7e0efe031b89f81cae6558dbf
Akash Hegde <akash.hegde@viacom18.com>
2024-01-22 11:15:09 +0530

    refactor byte count

--- cmd/ccwc/main.go
+++ cmd/ccwc/main.go
@@ -2,24 +2,43 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 )
 
-func main() {
-	var byteCount bool
-	flag.BoolVar(&byteCount, "c", false, "count bytes in the file")
-	flag.Parse()
+type fileStats struct {
+	byteCount      int
+	wordCount      int
+	linCount       int
+	characterCount int
+}
 
-	if len(os.Args) < 2 {
-		fmt.Println("file input missing")
-		os.Exit(1)
-	}
-	file := os.Args[2]
-	data, err := os.ReadFile(file)
+func getFileStats(filename string) *fileStats {
+	data, err := os.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(len(data), file)
+	return &fileStats{
+		byteCount: len(data),
+	}
+}
+
+func main() {
+	var byteCount, wordCount, lineCount, characterCount bool
+	flag.BoolVar(&byteCount, "c", false, "count bytes in the file")
+	flag.BoolVar(&wordCount, "w", false, "count words in the file")
+	flag.BoolVar(&lineCount, "l", false, "count lines in the file")
+	flag.BoolVar(&characterCount, "m", false, "count characters in the file")
+	flag.Parse()
+
+	filename := flag.CommandLine.Arg(0)
+
+	if !byteCount && !wordCount && !lineCount && !characterCount {
+		byteCount = true
+		wordCount = true
+		lineCount = true
+	}
+
+	fileStats := getFileStats(filename)
+	log.Println(fileStats.byteCount)
 }

b21270d048647dc6e927e359174ed959a4775f5d
Akash Hegde <akash99.hegde@gmail.com>
2024-01-23 22:01:26 +0530

    print filestats for line, word, len, char count

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ build:
 	go build -o ~/bin/ccwc cmd/ccwc/main.go
 
 run:
-	 go run cmd/ccwc/main.go -c test.txt
+	 go run cmd/ccwc/main.go -c -w -l -m test.txt
 
 hello:
 	echo "hello"
--- cmd/ccwc/main.go
+++ cmd/ccwc/main.go
@@ -1,26 +1,62 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"strconv"
+	"unicode"
 )
 
 type fileStats struct {
-	byteCount      int
-	wordCount      int
-	linCount       int
-	characterCount int
+	byteCount      int64
+	wordCount      int64
+	lineBreakCount int64
+	characterCount int64
 }
 
-func getFileStats(filename string) *fileStats {
-	data, err := os.ReadFile(filename)
-	if err != nil {
-		log.Fatal(err)
+func GetFileStats(file *os.File) fileStats {
+	var wordCount int64
+	var bytes int64
+	var lineBreakCount int64
+	var charsCount int64
+
+	reader := bufio.NewReader(file)
+	inWord := false
+
+	for {
+		c, sz, err := reader.ReadRune()
+
+		if err != nil {
+			if err == io.EOF {
+				if inWord {
+					wordCount++
+				}
+				break
+			} else {
+				log.Fatal(err)
+			}
+		}
+
+		bytes += int64(sz)
+		charsCount++
+
+		if unicode.IsSpace(c) {
+			if inWord {
+				wordCount++
+			}
+			if c == '\n' {
+				lineBreakCount++
+			}
+			inWord = false
+		} else {
+			inWord = true
+		}
 	}
-	return &fileStats{
-		byteCount: len(data),
-	}
+	return fileStats{byteCount: bytes, lineBreakCount: lineBreakCount, wordCount: wordCount, characterCount: charsCount}
 }
 
 func main() {
@@ -39,7 +75,38 @@ func main() {
 		lineCount = true
 	}
 
-	fileStats := getFileStats(filename)
-	log.Println(fileStats.byteCount)
-	log.Println("test1")
+	stat, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var file *os.File
+	if (stat.Mode() & os.ModeCharDevice) == 0 { // file through pipe operator
+		file = os.Stdin
+	} else { // file through cmd line
+		file, err = os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+	}
+
+	stats := GetFileStats(file)
+
+	output := ""
+
+	if wordCount {
+		output += strconv.FormatInt(stats.wordCount, 10) + " "
+	}
+	if lineCount {
+		output += strconv.FormatInt(stats.lineBreakCount, 10) + " "
+	}
+	if byteCount {
+		output += strconv.FormatInt(stats.byteCount, 10) + " "
+	}
+	if characterCount {
+		output += strconv.FormatInt(stats.characterCount, 10) + " "
+	}
+	output += filename
+	fmt.Println(output)
 }
